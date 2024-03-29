d5129e5b60f8de08330daef0e4c7a50fd9272053
Sunny9442 <sunnyshahzadsunny563@gmail.com>
2024-03-29 04:58:53 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+
+console.log("/n/ Welcome Sunny World (CLI number-guessing-game)/n/")
+
+const randomNumber = Math.floor (Math.random() * 10 + 3)
+
+const answer = await inquirer.prompt([
+    {
+        name : "userguessednumber",
+        type : "number",
+        message : "Enter you guessed Number with in (3 to 10)",
+    },
+]);
+console.log(answer)
+
+if (answer . userguessednumber === randomNumber){
+    console.log("congratulations you guessed a right number")
+}
+else {
+    console.log("Oh Sorry you guessed a wrong number")
+}
+
